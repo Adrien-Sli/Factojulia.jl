fa174b044931c931e5f3857a403c73c3df2df911
Adrien-Sli <adrien.slifirski@gmail.com>
2025-10-23 11:10:05 +0200

    première ébauche

--- src/acp.jl
+++ src/acp.jl
@@ -1,1 +1,26 @@
 say_hello() = print("hello") # test fonctionnement package
+
+"""
+Function that compute a standart pca
+
+Inputs : X should be either a 
+
+
+Outputs : results of the pca analysis
+- 
+- 
+-
+-
+-
+-
+-
+-
+-
+-
+-
+-
+"""
+function pca()
+    # compute pca
+    
+end
