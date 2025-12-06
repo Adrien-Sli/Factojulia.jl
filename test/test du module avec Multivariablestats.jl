5c434169829386036763c3737a0b7864e54c104c
Miller ABESSOLO MENGONG <abessolomengongmillerulrichjef@gmail.com>
2025-12-06 22:02:53 +0100

    Update test du module avec Multivariablestats.jl

--- test/test du module avec Multivariablestats.jl
+++ test/test du module avec Multivariablestats.jl
@@ -1,5 +1,7 @@
+using Pkg
+cd(@__DIR__)
 using .PCAApp
-res = run_pca("C:/Users/Lenovo/Downloads/student_exam_scores.csv")
+res = run_pca("student_exam_scores.csv")
 screeplot(res.pca)
 plot_individuals(res.scores)
 plot_variables(res.loadings, res.columns)
