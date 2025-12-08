8ced194b7c68b9ce9289d52069bf2d5937a3b778
Adrien-Sli <adrien.slifirski@gmail.com>
2025-12-08 14:12:08 +0100

    premier pas application

--- src/FactoJulia.jl
+++ src/FactoJulia.jl
@@ -1,14 +1,79 @@
 module FactoJulia
 
-# Packages
-using CSV, DataFrames, Statistics, LinearAlgebra, MultivariateStats
-using Bonito       # reactivity
-using Printf, Plots
+using Bonito
+using WGLMakie
+using Makie
+using CSV, DataFrames
 
-# Include PCA functions
 include("pca.jl")
+include("styles.jl")
+# using .ACP named module of pca.jl, maybe alright
 
-# Export the functions
-export PCA_, scree_plot, plot_PCA_individuals, plot_PCA_variables, print_matrix
+export run_app
+
+"""
+Fonction permettant de lancer l'application permettant la mise en oeuvre et l'affichage automatique des analyses factorielles
+
+    run_app() fonctionne par défaut sur le port local 8000
+"""
+function run_app(; port=8000)
+    # Elements
+    file_input   = Bonito.FileInput()
+    analysis_sel = Bonito.Dropdown(["ACP"])
+    run_button   = Bonito.Button("Afficher résultats", style = button_style)
+    fig_scree    = Bonito.Observable(nothing)
+    fig_ind      = Bonito.Observable(nothing)
+    fig_var      = Bonito.Observable(nothing)
+
+    # evenements
+    Bonito.on(run_button.value) do clicked
+        if clicked
+            # Placeholder figures
+            fig_scree[] = Makie.Figure(resolution=(400,300))
+            axis_s = Makie.Axis(fig_scree[], title="Scree plot placeholder")
+            Makie.text!(axis_s, 0.5, 0.5, "Scree plot", align = (:center, :center))
+
+            fig_ind[] = Makie.Figure(resolution=(400,300))
+            axis_i = Makie.Axis(fig_ind[], title="Individus placeholder")
+            Makie.text!(axis_i, 0.5, 0.5, "Individus", align = (:center, :center))
+
+            fig_var[] = Makie.Figure(resolution=(400,300))
+            axis_v = Makie.Axis(fig_var[], title="Variables placeholder")
+            Makie.text!(axis_v, 0.5, 0.5, "Variables", align = (:center, :center))
+
+            # Reset button
+            run_button.value[] = false
+        end
+    end
+
+    # ui
+ui = Bonito.DOM.div(
+    [
+        Bonito.DOM.div(Bonito.HTML("<h3>Selectionnez un ficher .csv</h3>")),    
+        file_input,
+        Bonito.DOM.div(Bonito.HTML("<h3>2. Choisissez l'analyse à effectuer</h3>")),
+        analysis_sel,
+        Bonito.DOM.div(Bonito.HTML("<h3>3. Affichez les résultats</h3>")),
+        run_button,
+        Bonito.map(fig_scree) do f
+            f isa Makie.Figure ? f : Bonito.HTML("")
+        end,
+        Bonito.map(fig_ind) do f
+            f isa Makie.Figure ? f : Bonito.HTML("")
+        end,
+        Bonito.map(fig_var) do f
+            f isa Makie.Figure ? f : Bonito.HTML("")
+        end
+    ]
+)
+
+
+    # APP & SERVER
+    app = Bonito.App(ui)
+    server = Bonito.Server(app, "127.0.0.1", port)
+
+    println("Server running at http://127.0.0.1:$port") # port local, à voir pr mettre en auto le port ip LAN du lanceur
+    return server
+end
 
 end
---
+++ src/styles.jl
@@ -0,0 +1,10 @@
+#styles
+button_style = Styles(
+    CSS("font-weight" => "500"),
+    CSS("color" => "black"),
+    CSS(":hover",
+        "background-color" => "green",
+        "box-shadow" => "rgba(0, 0, 0, 0) 0px 0px 0px 0px",
+        "color" => "white"
+    )
+)
